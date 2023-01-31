bbd5c3d2aedbba4870746407d1268775bc025295
Ming.Hu <Ming.Hu@noaa.gov>
2023-01-31 16:33:25 +0000

    Refactor utility to calculate ensemble mean and recenter:     1)  Use all cores to read; each core reads one or several 2D fields parallely from one member     2)  read member one by one; calculate mean and perturbation in each core; update member one by one     3)  file needs to have right chunk size (2D dimension).

--- ens_mean_recenter/ens_mean_recenter.f90
+++ ens_mean_recenter/ens_mean_recenter.f90
@@ -23,40 +23,50 @@ PROGRAM ens_mean_recenter
   use module_ncfile_stat, only : ncfile_stat
   use module_mpi_arrange, only : mpi_io_arrange
 
-  use netcdf, only: nf90_open,nf90_close,nf90_get_var,nf90_noerr
-  use netcdf, only: nf90_nowrite
+  use netcdf, only: nf90_open,nf90_close,nf90_noerr
+  use netcdf, only: nf90_get_var,nf90_put_var
+  use netcdf, only: nf90_nowrite,nf90_write
   use netcdf, only: nf90_inq_varid
-
+  use netcdf, only: nf90_strerror
 
   implicit none
 ! 
-  type(ncfile_stat) :: ncfs_dyn,ncfs_tracer,ncfs_sfc
+  type(ncfile_stat) :: ncfs_all
   type(mpi_io_arrange) :: mpiioarg
 !
 ! MPI variables
-  integer :: npe, mype, mypeLocal,ierror
+  integer :: npe, mype, ierror
+! sub communicator
+  integer :: new_comm
+  integer :: color, key
 !
 ! namelist
+  integer, parameter    :: max_num_file = 10
+  integer, parameter    :: filename_len=200
   integer :: fv3_io_layout_y
-  character(len=80)   :: sfcfile
-  character(len=80)   :: tracerfile
-  character(len=80)   :: dynfile
-  character(len=180)  :: varlist_sfc
-  character(len=180)  :: varlist_tracer
-  character(len=180)  :: varlist_dyn
-  integer :: numvar_sfc
-  integer :: numvar_tracer
-  integer :: numvar_dyn
-  integer :: num_ensemble
-  namelist/setup/ num_ensemble,fv3_io_layout_y, &
-                  sfcfile,numvar_sfc,varlist_sfc,&
-                  tracerfile,numvar_tracer,varlist_tracer,&
-                  dynfile,numvar_dyn,varlist_dyn
+  character(len=filename_len)  :: filebase
+  character(len=filename_len)  :: filetail(max_num_file)
+  integer                      :: numvar(max_num_file)
+  character(len=filename_len)  :: varlist(max_num_file)
+  integer    :: ens_size
+  logical    :: l_write_mean             ! if write ensmeble mean
+  logical    :: l_recenter               ! if recenter
+  real       :: beta
+
+  namelist/setup/ ens_size,fv3_io_layout_y,l_write_mean,l_recenter, &
+                  filebase,filetail,&
+                  numvar,varlist,beta
+
+  character (len=filename_len)   :: directory                 ! General filename stub.
+  character (len=filename_len)   :: filename                  ! General filename stub.
+  character (len=filename_len)   :: input_file(max_num_file)
+  character (len=filename_len)   :: input_control_file(max_num_file)
+  character (len=filename_len)   :: input_ensmean_file(max_num_file)
+  integer :: totalnumfiles
+
   logical :: ifexist
 !
 ! MPI distribution array
-  character(len=80)   :: filelist(3)
-!
   integer :: mype_fileid
   character(len=20) :: mype_varname
   integer :: mype_vartype
@@ -64,19 +74,21 @@ PROGRAM ens_mean_recenter
   integer :: mype_lbegin,mype_lend
 !
 ! array
-  integer :: mype_nz
   real(4),allocatable :: tmpd3r4(:,:,:)
   real(8),allocatable :: tmpd3r8(:,:,:)
 
-  real(4),allocatable :: d3r4_mean(:,:,:)
+  real(8),allocatable :: d3r8_mean(:,:,:)
   real(4),allocatable :: d4r4(:,:,:,:)
+
   integer :: startloc(3)
   integer :: countloc(3)
   integer :: ncioid,var_id
 !
 !
   integer :: i,j,k,iret,ilev,iens
-  real :: average
+  real(8) :: average
+  character (len=3)    :: memnun 
+  logical :: l_positive
 !
 !**********************************************************************
 !**********************************************************************
@@ -87,21 +99,18 @@ PROGRAM ens_mean_recenter
   call MPI_INIT(ierror)
   call MPI_COMM_SIZE(mpi_comm_world,npe,ierror)
   call MPI_COMM_RANK(mpi_comm_world,mype,ierror)
-
 !
 !  get namelist
 !
-  num_ensemble=1
+  numvar=0
+  varlist=''
+  filetail=''
+  ens_size=1
   fv3_io_layout_y=1
-  sfcfile="sfc_data.nc"
-  numvar_sfc=0
-  varlist_sfc="2mt"
-  tracerfile="fv_tracer.res.tile1.nc"
-  numvar_tracer=13
-  varlist_tracer="sphum liq_wat ice_wat rainwat snowwat graupel water_nc ice_nc rain_nc o3mr liq_aero ice_aero sgs_tke"
-  dynfile="fv_core.res.tile1.nc"
-  numvar_dyn=7
-  varlist_dyn="u v W DZ T delp phis"
+  filebase='fv3sar_tile1'
+  l_write_mean=.true.
+  l_recenter=.false.
+  beta=1.0
 
   inquire(file='namelist.ens', EXIST=ifexist )
   if(ifexist) then
@@ -117,30 +126,44 @@ PROGRAM ens_mean_recenter
     write(*,setup)
   endif
 
-  filelist(1)=trim(dynfile)
-  filelist(2)=trim(tracerfile)
-  filelist(3)=trim(sfcfile)
-  average=1.0/float(num_ensemble)
+  !filename = trim(adjustl(directory)) // trim(adjustl(filebase))
+  filename = trim(adjustl(filebase))
+! decide how many files we will process
+  totalnumfiles=0
+  do i=1,max_num_file
+    if(numvar(i) > 0) then
+       totalnumfiles=totalnumfiles+1
+    endif
+  enddo
+! decide control file name and ensemble mean file name
+  do i=1,totalnumfiles
+     input_control_file(i) = 'control_'//trim(filetail(i))
+     input_ensmean_file(i) = trim(adjustl(filebase))//'_'//trim(filetail(i))
+  enddo
+  if(l_recenter) then
+     do i=1,totalnumfiles
+        input_file(i) = input_control_file(i)
+     end do
+  else
+     do i=1,totalnumfiles
+        input_file(i) = input_ensmean_file(i)
+     enddo
+  endif
 !
   if(mype==0) then
 !
 ! find dimension of each field
 !
-     call ncfs_tracer%init(trim(tracerfile),numvar_tracer,trim(varlist_tracer))
-     call ncfs_tracer%fill_dims()
-     call ncfs_dyn%init(trim(dynfile),numvar_dyn,trim(varlist_dyn))
-     call ncfs_dyn%fill_dims()
-     call ncfs_sfc%init(trim(sfcfile),numvar_sfc,trim(varlist_sfc))
-     call ncfs_sfc%fill_dims()
+     call ncfs_all%init(totalnumfiles,input_file(1:totalnumfiles), &
+                        numvar(1:totalnumfiles),varlist(1:totalnumfiles))
+     call ncfs_all%fill_dims()
 !
 !  distibute variables to each core
 !
      call mpiioarg%init(npe)
-     call mpiioarg%arrange(ncfs_dyn,ncfs_tracer,ncfs_sfc)
+     call mpiioarg%arrange(ncfs_all)
          
-     call ncfs_tracer%close()
-     call ncfs_dyn%close()
-     call ncfs_sfc%close()
+     call ncfs_all%close()
   endif
 
   call MPI_Scatter(mpiioarg%fileid, 1, mpi_integer, mype_fileid, 1, mpi_integer, 0, MPI_COMM_WORLD,ierror)
@@ -152,13 +175,25 @@ PROGRAM ens_mean_recenter
   call MPI_Scatter(mpiioarg%lvlend, 1, mpi_integer, mype_lend, 1, mpi_integer, 0, MPI_COMM_WORLD,ierror)
 
   if(mype==0) call mpiioarg%close()
-
-!  if(mype==0) write(*,*) 
   call mpi_barrier(MPI_COMM_WORLD,ierror)
-!  write(6,'(2I5,2x,a10,10I10)') mype+1,mype_fileid,mype_varname,mype_vartype,mype_nx,mype_ny,mype_lbegin,mype_lend
 
-  if(mype_fileid > 0) then
-     mype_nz=mype_lend-mype_lbegin+1
+! Create sub-communicator to handle each file
+  key=mype+1
+  if(mype_fileid > 0 .and. mype_fileid <= totalnumfiles) then
+     color = mype_fileid
+  else
+     color = MPI_UNDEFINED
+  endif
+
+  call MPI_Comm_split(mpi_comm_world,color,key,new_comm,ierror)
+  if ( ierror /= 0 ) then
+     write(6,'(a,i5)')'***ERROR*** after mpi_comm_create with iret = ',ierror
+     call mpi_abort(mpi_comm_world,101,ierror)
+  endif
+!
+! read 2D field from each file using sub communicator
+!
+  if (MPI_COMM_NULL /= new_comm) then
      if(mype_vartype==5) then
         allocate(tmpd3r4(mype_nx,mype_ny,1))
      elseif(mype_vartype==6) then
@@ -166,17 +201,24 @@ PROGRAM ens_mean_recenter
      else
         write(6,*) 'Warning, unknown datatype'
      endif
-     allocate(d3r4_mean(mype_nx,mype_ny,mype_lbegin:mype_lend))
-     allocate(d4r4(mype_nx,mype_ny,mype_lbegin:mype_lend,num_ensemble))
-
+     allocate(d4r4(mype_nx,mype_ny,mype_lbegin:mype_lend,0:ens_size))
+!
 ! now read in each fields from fv3 file
-
-     d3r4_mean=0.0
+!
      d4r4=0.0
-     do iens=1,num_ensemble
-        iret=nf90_open(filelist(mype_fileid),nf90_nowrite,ncioid,comm=mpi_comm_world,info=MPI_INFO_NULL)
+     do iens=0,ens_size
+
+        if(iens==0) then
+           ! save control file as ensemble 0
+           filename = trim(input_file(mype_fileid))
+        else
+           write(UNIT=memnun,FMT='(i3.3)') iens
+           filename = trim(adjustl(filebase))//'_mem'//memnun//'_'//trim(filetail(mype_fileid))
+        endif
+        iret=nf90_open(trim(filename),nf90_nowrite,ncioid,comm=new_comm,info=MPI_INFO_NULL)
         if(iret/=nf90_noerr) then
-           write(6,*)' problem opening', trim(filelist(mype_fileid)),', Status =',iret
+           write(6,*)' problem opening ', trim(filename),' fileid=',mype_fileid,', Status =',iret
+           write(6,*)  nf90_strerror(iret)
            call flush(6)
            stop(333)
         endif
@@ -189,24 +231,126 @@ PROGRAM ens_mean_recenter
            iret=nf90_inq_varid(ncioid,trim(adjustl(mype_varname)),var_id)
            if(mype_vartype==5) then
               iret=nf90_get_var(ncioid,var_id,tmpd3r4,start=startloc,count=countloc)
-              d3r4_mean(:,:,ilev)=d3r4_mean(:,:,ilev)+tmpd3r4(:,:,1)
               d4r4(:,:,ilev,iens)=tmpd3r4(:,:,1)
            elseif(mype_vartype==6) then
               iret=nf90_get_var(ncioid,var_id,tmpd3r8,start=startloc,count=countloc)
-              d3r4_mean(:,:,ilev)=d3r4_mean(:,:,ilev)+tmpd3r8(:,:,1)
               d4r4(:,:,ilev,iens)=tmpd3r8(:,:,1)
            endif
         enddo  ! ilev
 
         iret=nf90_close(ncioid)
 
-       ! do ilev=mype_lbegin,mype_lend
-       !    write(6,'(I5,A10,2I5,2f15.7)') mype_fileid,trim(adjustl(mype_varname)),ilev,iens,  &
-       !            maxval(d4r4(:,:,ilev,iens)),minval(d4r4(:,:,ilev,iens))
-       ! enddo  ! ilev
-
      enddo ! iens
 !
+!  calculate ensemble mean
+!
+     if(mype==0) write(*,*) 'calculate ensemble mean'
+     allocate(d3r8_mean(mype_nx,mype_ny,mype_lbegin:mype_lend))
+     d3r8_mean=0.0_8
+     do iens=1,ens_size
+        do ilev=mype_lbegin,mype_lend
+           d3r8_mean(:,:,ilev)=d3r8_mean(:,:,ilev)+d4r4(:,:,ilev,iens)
+        enddo
+     enddo
+     average=ens_size
+     average=1.0_8/average
+     d3r8_mean=d3r8_mean*average
+!
+!     do ilev=mype_lbegin,mype_lend
+!        write(6,'(I5,A10,I5,2f25.9)') mype_fileid,trim(adjustl(mype_varname)),ilev,  &
+!                   maxval(d3r8_mean(:,:,ilev)),minval(d3r8_mean(:,:,ilev))
+!     enddo  ! ilev
+! write mean
+     if( l_write_mean ) then
+        filename = trim(input_ensmean_file(mype_fileid))
+        iret=nf90_open(trim(filename),nf90_write,ncioid,comm=new_comm,info=MPI_INFO_NULL)
+        if(iret/=nf90_noerr) then
+           write(6,*)' problem opening ', trim(filename),' fileid=',mype_fileid,', Status =',iret
+           write(6,*)  nf90_strerror(iret)
+           call flush(6)
+           stop(555)
+        endif
+        if(mype==0) write(*,*) 'write ensemble mean = ', &
+                                trim(input_ensmean_file(mype_fileid))
+
+        do ilev=mype_lbegin,mype_lend
+           startloc=(/1,1,ilev/)
+           countloc=(/mype_nx,mype_ny,1/)
+
+           iret=nf90_inq_varid(ncioid,trim(adjustl(mype_varname)),var_id)
+           if(mype_vartype==5) then
+              tmpd3r4(:,:,1)=d3r8_mean(:,:,ilev)
+              iret=nf90_put_var(ncioid,var_id,tmpd3r4,start=startloc,count=countloc)
+           elseif(mype_vartype==6) then
+              tmpd3r8(:,:,1)=d3r8_mean(:,:,ilev)
+              iret=nf90_put_var(ncioid,var_id,tmpd3r8,start=startloc,count=countloc)
+           endif
+        enddo  ! ilev
+
+        iret=nf90_close(ncioid)
+     endif
+!
+! recenter  
+!
+     if(l_recenter) then
+! get delta 
+        l_positive=.false.
+        if( trim(mype_varname)=="ref_f3d" .or. trim(mype_varname)=="sphum" .or.  &
+           trim(mype_varname)=="liq_wat" .or. trim(mype_varname)=="snowwat" .or. & 
+           trim(mype_varname)=="rainwat" .or. trim(mype_varname)=="ice_wat" .or.&
+           trim(mype_varname)=="graupel" )then
+           l_positive=.true.
+        endif
+
+        do ilev=mype_lbegin,mype_lend
+           d3r8_mean(:,:,ilev)=d4r4(:,:,ilev,0)-d3r8_mean(:,:,ilev)
+        enddo
+        do iens=1,ens_size
+           do ilev=mype_lbegin,mype_lend
+              d4r4(:,:,ilev,iens)=d4r4(:,:,ilev,iens)+d3r8_mean(:,:,ilev)
+              if(l_positive) d4r4(:,:,ilev,iens)=max(d4r4(:,:,ilev,iens), 0.0)
+           enddo
+        enddo
+!
+!  write to each member
+!
+        do iens=1,ens_size
+
+           write(UNIT=memnun,FMT='(i3.3)') iens
+           filename = 'rec_'//trim(adjustl(filebase))//'_mem'//memnun//'_'//trim(filetail(mype_fileid))
+           iret=nf90_open(trim(filename),nf90_write,ncioid,comm=new_comm,info=MPI_INFO_NULL)
+           if(iret/=nf90_noerr) then
+              write(6,*)' problem opening ', trim(filename),' fileid=',mype_fileid,', Status =',iret
+              write(6,*)  nf90_strerror(iret)
+              call flush(6)
+              stop(444)
+           endif
+           if(mype==0) write(*,*) 'update ensemble member =',iens
+
+           do ilev=mype_lbegin,mype_lend
+              startloc=(/1,1,ilev/)
+              countloc=(/mype_nx,mype_ny,1/)
+
+              iret=nf90_inq_varid(ncioid,trim(adjustl(mype_varname)),var_id)
+              if(mype_vartype==5) then
+                 tmpd3r4(:,:,1)=d4r4(:,:,ilev,iens)
+                 iret=nf90_put_var(ncioid,var_id,tmpd3r4,start=startloc,count=countloc)
+              elseif(mype_vartype==6) then
+                 tmpd3r8(:,:,1)=d4r4(:,:,ilev,iens)
+                 iret=nf90_put_var(ncioid,var_id,tmpd3r8,start=startloc,count=countloc)
+              endif
+           enddo  ! ilev
+
+           iret=nf90_close(ncioid)
+
+        enddo ! iens
+
+     endif
+
+! release memory
+     deallocate(d3r8_mean)
+     deallocate(d4r4)
+
      if(mype_vartype==5) then
         deallocate(tmpd3r4)
      elseif(mype_vartype==6) then
@@ -214,24 +358,15 @@ PROGRAM ens_mean_recenter
      else
         write(6,*) 'Warning, unknown datatype'
      endif
-!
-!  calculate ensemble mean
-!
-     d3r4_mean=d3r4_mean*average
-     do ilev=mype_lbegin,mype_lend
-        write(6,'(I5,A10,I5,2f15.7)') mype_fileid,trim(adjustl(mype_varname)),ilev,  &
-                   maxval(d3r4_mean(:,:,ilev)),minval(d3r4_mean(:,:,ilev))
-     enddo  ! ilev
-
-     deallocate(d3r4_mean)
-     deallocate(d4r4)
-
   endif
 
+  if (MPI_COMM_NULL /= new_comm) then
+     call MPI_Comm_free(new_comm,iret)
+  endif
+!
   call mpi_barrier(MPI_COMM_WORLD,ierror)
   if(mype==0)  write(6,*) "=== RRFS ENS MEAN and RECENTER SUCCESS ==="
   call MPI_FINALIZE(ierror)
 !
-
 END PROGRAM ens_mean_recenter
 
--- ens_mean_recenter/module_mpi_arrange.f90
+++ ens_mean_recenter/module_mpi_arrange.f90
@@ -96,7 +96,7 @@ contains
 
   end subroutine close
 
-  subroutine arrange(this,ncfs_dyn,ncfs_tracer,ncfs_sfc)
+  subroutine arrange(this,ncfs_all)
 !                .      .    .                                       .
 ! subprogram: 
 !   prgmmr:
@@ -113,9 +113,7 @@ contains
     implicit none
 
     class(mpi_io_arrange) :: this
-    type(ncfile_stat),intent(in) :: ncfs_dyn
-    type(ncfile_stat),intent(in) :: ncfs_tracer
-    type(ncfile_stat),intent(in) :: ncfs_sfc
+    type(ncfile_stat),intent(in) :: ncfs_all
 !
 ! MPI distribution array
     integer,allocatable :: fileid(:)
@@ -124,13 +122,15 @@ contains
     integer,allocatable :: lvlbegin(:),lvlend(:)
     integer,allocatable :: nx(:),ny(:)
     integer :: ntotalcore
-    integer :: nlvl2d,nlvl3d
+    integer :: nlvl2d,nlvl3d,nlvl3d_small
     integer :: nlvlcore
     integer :: nlvlmax
     integer,allocatable :: nlvl3d_list(:)
     integer :: mynlvl3d
 !
-    integer :: i,j,k,nn,n3d,nz
+    integer :: myfieldid
+    integer,allocatable :: ib(:),ie(:)
+    integer :: i,j,k,n,nn,n3d,nz
 !
 ! start  allocate memory
 !
@@ -142,174 +142,139 @@ contains
     allocate(lvlend(ntotalcore))
     allocate(nx(ntotalcore))
     allocate(ny(ntotalcore))
+    varname=""
+    fileid=0
+    vartype=0
+    lvlbegin=0
+    lvlend=0
+    nx=0
+    ny=0
 !
 ! find number of 3d and 2 d fields
 !
     nlvl2d=0
     nlvl3d=0
+    nlvl3d_small=0
     nlvlmax=0
-    if(ncfs_tracer%num_totalvl > 0) then
-        do k=1,ncfs_tracer%numvar
-           if(ncfs_tracer%num_dim(k) == 3) nlvl3d=nlvl3d+1
-           if(ncfs_tracer%num_dim(k) == 2) nlvl2d=nlvl2d+1
-           if(nlvlmax < ncfs_tracer%dim_3(k)) nlvlmax=ncfs_tracer%dim_3(k)
+    if(ncfs_all%num_totalvl > 0) then
+        do k=1,ncfs_all%numvar
+           if(ncfs_all%num_dim(k) == 3) then
+              if(ncfs_all%dim_3(k) <= 10) then
+                ! treat small 3d as 2D field
+                nlvl3d_small=nlvl3d_small+ncfs_all%dim_3(k)
+              else
+                nlvl3d=nlvl3d+1
+              endif
+           endif
+           if(ncfs_all%num_dim(k) == 2) nlvl2d=nlvl2d+1
+           if(nlvlmax < ncfs_all%dim_3(k)) nlvlmax=ncfs_all%dim_3(k)
         enddo
     endif
-    if(ncfs_dyn%num_totalvl > 0) then
-        do k=1,ncfs_dyn%numvar
-           if(ncfs_dyn%num_dim(k) == 3) nlvl3d=nlvl3d+1
-           if(ncfs_dyn%num_dim(k) == 2) nlvl2d=nlvl2d+1
-           if(nlvlmax < ncfs_dyn%dim_3(k)) nlvlmax=ncfs_dyn%dim_3(k)
-        enddo
-    endif
-    if(ncfs_sfc%num_totalvl > 0) then
-        do k=1,ncfs_sfc%numvar
-           if(ncfs_sfc%num_dim(k) == 3) nlvl3d=nlvl3d+1
-           if(ncfs_sfc%num_dim(k) == 2) nlvl2d=nlvl2d+1
-           if(nlvlmax < ncfs_sfc%dim_3(k)) nlvlmax=ncfs_sfc%dim_3(k)
-        enddo
-    endif
-    write(6,*) 'total 2d level=',nlvl2d,' 3d level=',nlvl3d
+    write(6,*) 'total 2d_level=',nlvl2d,' 3d_level=',nlvl3d, &
+               ' 3d_level_small =',nlvl3d_small
     write(6,*) 'max level =',nlvlmax
 !
 !  check if we have enought cores
 !
-    if( ntotalcore-nlvl2d < nlvl3d ) then
-        write(6,*) 'not enough cores for the paralleli IO',ntotalcore-nlvl2d,nlvl3d
+    if( ntotalcore-nlvl2d-nlvl3d_small < nlvl3d ) then
+        write(6,*) 'not enough cores for the paralleli IO',ntotalcore-nlvl2d-nlvl3d_small,nlvl3d
         stop 123
     endif
 !
 ! decide how many cores can be used for each 3d field
 !
-    allocate(nlvl3d_list(nlvl3d))
-    nlvlcore=(ntotalcore-nlvl2d)/nlvl3d
-    nlvl3d_list=nlvlcore
-    nlvlcore=(ntotalcore-nlvl2d) - nlvl3d*nlvlcore 
-    if(nlvlcore > 0 ) then
-        do k=1,nlvlcore
-          nlvl3d_list(k)=nlvl3d_list(k)+1
-        enddo
-    endif
-    write(6,*) nlvl3d_list
+    if(nlvl3d > 0) then
+      allocate(nlvl3d_list(nlvl3d))
+      nlvlcore=(ntotalcore-nlvl2d-nlvl3d_small)/nlvl3d
+      nlvl3d_list=nlvlcore
+      nlvlcore=(ntotalcore-nlvl2d-nlvl3d_small) - nlvl3d*nlvlcore 
+      if(nlvlcore > 0 ) then
+          do k=1,nlvlcore
+            nlvl3d_list(k)=nlvl3d_list(k)+1
+          enddo
+      endif
+      write(6,*) 'cores for each 3D fields=',nlvl3d_list
+   endif
+!
+!  decide boundary for fileid
+!
+    allocate(ib(ncfs_all%numfiles),ie(ncfs_all%numfiles))    
+    ib=0
+    ie=0
+    ib(1)=1
+    ie(1)=ncfs_all%numvarfile(1)
+    do i=2,ncfs_all%numfiles
+      ib(i)=ie(i-1)+1
+      ie(i)=ib(i)+ncfs_all%numvarfile(i)-1
+    enddo
 !
 !  decide which levels of a variable to read for each core
 !
     nn=0
     n3d=0
-    if(ncfs_dyn%numvar > 0) then
-        do i=1,ncfs_dyn%numvar
-           if(ncfs_dyn%num_dim(i)==3) then
-              nz=ncfs_dyn%dim_3(i)
-              n3d=n3d+1
-              mynlvl3d=min(nlvl3d_list(n3d),nz)
-              nlvlcore=nz/mynlvl3d
-              j=nz-nlvlcore*mynlvl3d
-              do k=1,mynlvl3d
-                 nn=nn+1
-                 fileid(nn)=1
-                 varname(nn)=trim(ncfs_dyn%list_varname(i))
-                 vartype(nn)=ncfs_dyn%vartype(i)
-                 nx(nn)=ncfs_dyn%dim_1(i)
-                 ny(nn)=ncfs_dyn%dim_2(i)
-                 if(k==1) then
-                    lvlbegin(nn)=1
-                 else
-                    lvlbegin(nn)=lvlend(nn-1)+1
-                 endif
-                 lvlend(nn)=lvlbegin(nn)+nlvlcore-1
-                 if(k<=j) lvlend(nn)=lvlend(nn)+1
-                 if(k==mynlvl3d) lvlend(nn)=min(lvlend(nn),nz)
-              enddo
-           elseif(ncfs_dyn%num_dim(i)==2) then
+    if(ncfs_all%numvar > 0) then
+        do i=1,ncfs_all%numvar
+           myfieldid=0
+           do n =1, ncfs_all%numfiles
+              if(i >= ib(n) .and. i <= ie(n)) myfieldid=n
+           enddo
+           if(myfieldid <=0 .or. myfieldid > ncfs_all%numfiles) then
+              write(6,*) 'something wrong with file id ',myfieldid
+              stop 234
+           endif
+           if(ncfs_all%num_dim(i)==3) then
+              nz=ncfs_all%dim_3(i)
+              if(nz <= 10) then
+                 do k=1,nz
+                    nn=nn+1
+                    varname(nn)=trim(ncfs_all%list_varname(i))
+                    vartype(nn)=ncfs_all%vartype(i)
+                    nx(nn)=ncfs_all%dim_1(i)
+                    ny(nn)=ncfs_all%dim_2(i)
+                    lvlbegin(nn)=k
+                    lvlend(nn)=k
+                    fileid(nn)=myfieldid
+                 enddo
+              else
+                 n3d=n3d+1
+                 mynlvl3d=min(nlvl3d_list(n3d),nz)
+                 nlvlcore=nz/mynlvl3d
+                 j=nz-nlvlcore*mynlvl3d
+                 do k=1,mynlvl3d
+                    nn=nn+1
+                    fileid(nn)=myfieldid
+                    varname(nn)=trim(ncfs_all%list_varname(i))
+                    vartype(nn)=ncfs_all%vartype(i)
+                    nx(nn)=ncfs_all%dim_1(i)
+                    ny(nn)=ncfs_all%dim_2(i)
+                    if(k==1) then
+                       lvlbegin(nn)=1
+                    else
+                       lvlbegin(nn)=lvlend(nn-1)+1
+                    endif
+                    lvlend(nn)=lvlbegin(nn)+nlvlcore-1
+                    if(k<=j) lvlend(nn)=lvlend(nn)+1
+                    if(k==mynlvl3d) lvlend(nn)=min(lvlend(nn),nz)
+                 enddo
+              endif
+           elseif(ncfs_all%num_dim(i)==2) then
               nn=nn+1
-              varname(nn)=trim(ncfs_dyn%list_varname(i))
-              vartype(nn)=ncfs_dyn%vartype(i)
-              nx(nn)=ncfs_dyn%dim_1(i)
-              ny(nn)=ncfs_dyn%dim_2(i)
+              varname(nn)=trim(ncfs_all%list_varname(i))
+              vartype(nn)=ncfs_all%vartype(i)
+              nx(nn)=ncfs_all%dim_1(i)
+              ny(nn)=ncfs_all%dim_2(i)
               lvlbegin(nn)=1
               lvlend(nn)=1
-              fileid(nn)=1
+              fileid(nn)=myfieldid
            endif
         enddo
     endif
 
-    if(ncfs_tracer%numvar > 0) then
-        do i=1,ncfs_tracer%numvar
-           if(ncfs_tracer%num_dim(i)==3) then
-              nz=ncfs_tracer%dim_3(i)
-              n3d=n3d+1
-              mynlvl3d=min(nlvl3d_list(n3d),nz)
-              nlvlcore=nz/mynlvl3d
-              j=nz-nlvlcore*mynlvl3d
-              do k=1,mynlvl3d
-                 nn=nn+1
-                 fileid(nn)=2
-                 varname(nn)=trim(ncfs_tracer%list_varname(i))
-                 vartype(nn)=ncfs_tracer%vartype(i)
-                 nx(nn)=ncfs_tracer%dim_1(i)
-                 ny(nn)=ncfs_tracer%dim_2(i)
-                 if(k==1) then
-                    lvlbegin(nn)=1
-                 else
-                    lvlbegin(nn)=lvlend(nn-1)+1
-                 endif
-                 lvlend(nn)=lvlbegin(nn)+nlvlcore-1
-                 if(k<=j) lvlend(nn)=lvlend(nn)+1
-                 if(k==mynlvl3d) lvlend(nn)=min(lvlend(nn),nz)
-              enddo
-           elseif(ncfs_tracer%num_dim(i)==2) then
-              nn=nn+1
-              varname(nn)=trim(ncfs_tracer%list_varname(i))
-              vartype(nn)=ncfs_tracer%vartype(i)
-              nx(nn)=ncfs_tracer%dim_1(i)
-              ny(nn)=ncfs_tracer%dim_2(i)
-              lvlbegin(nn)=1
-              lvlend(nn)=1
-              fileid(nn)=2
-           endif
-        enddo
-    endif
-
-    if(ncfs_sfc%numvar > 0) then
-        do i=1,ncfs_sfc%numvar
-           if(ncfs_sfc%num_dim(i)==3) then
-              nz=ncfs_sfc%dim_3(i)
-              n3d=n3d+1
-              mynlvl3d=min(nlvl3d_list(n3d),nz)
-              nlvlcore=nz/mynlvl3d
-              j=nz-nlvlcore*mynlvl3d
-              do k=1,mynlvl3d
-                 nn=nn+1
-                 fileid(nn)=3
-                 varname(nn)=trim(ncfs_sfc%list_varname(i))
-                 vartype(nn)=ncfs_sfc%vartype(i)
-                 nx(nn)=ncfs_sfc%dim_1(i)
-                 ny(nn)=ncfs_sfc%dim_2(i)
-                 if(k==1) then
-                    lvlbegin(nn)=1
-                 else
-                    lvlbegin(nn)=lvlend(nn-1)+1
-                 endif
-                 lvlend(nn)=lvlbegin(nn)+nlvlcore-1
-                 if(k<=j) lvlend(nn)=lvlend(nn)+1
-                 if(k==mynlvl3d) lvlend(nn)=min(lvlend(nn),nz)
-              enddo
-           elseif(ncfs_sfc%num_dim(i)==2) then
-              nn=nn+1
-              varname(nn)=trim(ncfs_sfc%list_varname(i))
-              vartype(nn)=ncfs_sfc%vartype(i)
-              nx(nn)=ncfs_sfc%dim_1(i)
-              ny(nn)=ncfs_sfc%dim_2(i)
-              lvlbegin(nn)=1
-              lvlend(nn)=1
-              fileid(nn)=3
-           endif
-        enddo
-    endif
-
+    write(6,'(2a5,2x,a10,10a10)') "core","fid","varname","vartype","nx","ny","lvlbegin","lvlend"
     do k=1,ntotalcore
-       write(6,'(2I5,2x,a10,10I10)') k,fileid(k),varname(k),vartype(k),nx(k),ny(k),lvlbegin(k),lvlend(k) 
+       write(6,'(2I5,2x,a10,10I10)') k,fileid(k),trim(varname(k)),vartype(k),nx(k),ny(k),lvlbegin(k),lvlend(k) 
     enddo
+    write(6,*) "======================================================================="
 !
 !  save results
 !
@@ -330,7 +295,9 @@ contains
     deallocate(lvlend)
     deallocate(nx)
     deallocate(ny)
-    deallocate(nlvl3d_list)
+    if(nlvl3d > 0) deallocate(nlvl3d_list)
+    deallocate(ib)
+    deallocate(ie)
 
   end subroutine arrange
 
