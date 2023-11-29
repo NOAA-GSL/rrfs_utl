4c1d9c4c547b5e66bc9f66f8364a0be1864315a0
Ruifang.Li <Ruifang.Li@noaa.gov>
2023-11-29 23:13:53 +0000

    PM converter bug fix, if station ID is not read by Z8 format, assign a new ID 99999999

--- pm/process_pm.f90
+++ pm/process_pm.f90
@@ -10,7 +10,7 @@ program pmbufr
  integer(8) :: z_sid
  character(80):: hdstr='SID XOB YOB DHR TYP T29 SQN PROCN RPT CAT TYPO TSIG'
  character(80):: obstr='TPHR QCIND COPOPM'
- character(8) :: subset,c_sid
+ character(8) :: subset,c_sid,c_sid_org
  character(10):: analysis_time
  character(20):: str,infile,outfile
  character(20),allocatable,dimension(:)::sta_id, lat_str, lon_str, elv_str, pm_aqi_str, pm_measured_str, pm_str
@@ -100,11 +100,12 @@ program pmbufr
 
     str=adjustl(sta_id(j))
     c_sid=str(2:9)
+    c_sid_org=c_sid
     read(c_sid,'(z8)',iostat=ios) z_sid
     if (ios/=0) then 
-      c_sid="9999999"
+      c_sid="99999999"
       read(c_sid,'(z8)',iostat=ios) z_sid
-      if (ios/=0) write (*,'(a,a25)') 'wrong station_id ', str
+      if (ios/=0) write (*,'(a,2a15)') 'wrong station_id ', c_sid_org,c_sid
     end if  
     
     hdr(1)=rstation_id; hdr(2)=lon; hdr(3)=lat; hdr(4)=0.0_8; hdr(5)=102; hdr(10)=6 ! Single level report
@@ -116,15 +117,15 @@ program pmbufr
     if (pm > 0.0) then
       obs(2,nlvl)=0.0_8
       obs(3,nlvl)=pm*1e-9_8  ! "UG/M3" to KG/M3
-      write(*,'(a25,i7,3x,2a14,3f10.2,f15.12)') 'goodObs: pm>0',j,str,c_sid,lat,lon,obs(2,nlvl),obs(3,nlvl)
+      write(*,'(a25,i7,3x,2a14,3f10.2,f15.12)') 'goodObs: pm>0',j,c_sid_org,c_sid,lat,lon,obs(2,nlvl),obs(3,nlvl)
     else if (pm > pm_limit) then
       obs(2,nlvl)=0.0_8
       obs(3,nlvl)=0.0_8
-      write(*,'(a25,i7,3x,2a14,3f10.2,f15.12)') 'limitObs: -15<pm<0',j,str,c_sid,lat,lon,obs(2,nlvl),obs(3,nlvl)
+      write(*,'(a25,i7,3x,2a14,3f10.2,f15.12)') 'limitObs: -15<pm<0',j,c_sid_org,c_sid,lat,lon,obs(2,nlvl),obs(3,nlvl)
     else
       obs(2,nlvl)=bmiss
       obs(3,nlvl)=bmiss
-      write(*,'(a25,i7,3x,2a14,3f10.2,f15.12)') 'badObs: pm<-15',j,str,c_sid,lat,lon,obs(2,nlvl),obs(3,nlvl)
+      write(*,'(a25,i7,3x,2a14,3f10.2,f15.12)') 'badObs: pm<-15',j,c_sid_org,c_sid,lat,lon,obs(2,nlvl),obs(3,nlvl)
     endif
 
     ! only encode hdr and obs, no err and qc since these are not defined in pm.bufrtable 
