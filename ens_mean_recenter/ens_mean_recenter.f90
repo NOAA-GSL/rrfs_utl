ca6b673014e32433f0863a29adc44edc58f2af45
chunhua zhou <chunhua.zhou@noaa.gov>
2023-11-30 15:33:48 -0700

    Increase the length of varlist to hold more variables for recentering (#54)
    * Increase the length of varlist to hold more variables for recentering
    
    * just use 500 for the varlist length

--- ens_mean_recenter/ens_mean_recenter.f90
+++ ens_mean_recenter/ens_mean_recenter.f90
@@ -47,7 +47,7 @@ PROGRAM ens_mean_recenter
   character(len=filename_len)  :: filebase
   character(len=filename_len)  :: filetail(max_num_file)
   integer                      :: numvar(max_num_file)
-  character(len=filename_len)  :: varlist(max_num_file)
+  character(len=500)  :: varlist(max_num_file)
   integer    :: ens_size
   logical    :: l_write_mean             ! if write ensmeble mean
   logical    :: l_recenter               ! if recenter
